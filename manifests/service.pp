0f248e4fc56dc4ea9114e72a00d286d8c07660d4
Shaun Martin <shaunm@airg.com>
2014-07-22 20:59:30 +0000

    Parameterized service class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,16 +1,25 @@
-class haproxy::service {
+class haproxy::service
+(
+    $service_name,
+    $service_enable,
+    $service_ensure,
+    $service_reload,
+)
+{
 
-  service {$haproxy::params::service_name :
-    ensure      => $haproxy::service_ensure,
-    enable      => $haproxy::service_enable,
-    hasrestart  => true,
-    hasstatus   => true,
-  }
-
-  exec {"${haproxy::params::service_name} reload":
-    command     => "/etc/init.d/${haproxy::params::service_name} reload",
-    refreshonly => true,
-    subscribe   => [ Concat_build['haproxy'], File["${haproxy::params::config_dir}subnet_softec.lst"] ]
-  }
+	service { $service_name :
+		ensure		=> $service_ensure,
+		enable		=> $service_enable,
+		hasrestart  => true,
+		hasstatus	=> true,
+	}
+    
+    if $service_reload {
+    	exec {"${service_name} reload":
+	    	command		=> "/etc/init.d/${service_name} reload",
+		    refreshonly => true,
+    		subscribe	=> [ Concat_build['haproxy'], File["${config_dir}subnet_softec.lst"] ]
+	    }
+    }
 
 }
