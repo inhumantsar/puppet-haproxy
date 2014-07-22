a78efd21a5374fbaf2e5a3c9b6d6cd7f9513c0b5
Shaun Martin <shaunm@airg.com>
2014-07-22 21:24:56 +0000

    Parameterized logrotate

--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -2,22 +2,22 @@
 #
 # This class enable logorotate of logfile created by haproxy
 #
-#
-#
-#
-#
-class haproxy::logrotate {
-  logrotate::file { 'haproxy':
-    log           =>  "${haproxy::log_dir}/*.log",
-    interval      =>  'daily',
-    rotation      =>  '930',
-    options       =>  [ 'missingok', 'compress', 'notifempty', 'sharedscripts' ],
-    archive       =>  true,
-    olddir        =>  "${haproxy::log_dir}/archives",
-    olddir_owner  =>  'root',
-    olddir_group  =>  'users',
-    olddir_mode   =>  '655',
-    create        =>  '640 syslog adm',
-    postrotate    =>  'invoke-rc.d rsyslog reload',
-  }
+class haproxy::logrotate
+(
+    $log_dir,
+)
+{
+	logrotate::file { 'haproxy':
+		log             => "${log_dir}/*.log",
+		interval		=> 'daily',
+		rotation		=> '930',
+		options			=> [ 'missingok', 'compress', 'notifempty', 'sharedscripts' ],
+		archive			=> true,
+		olddir			=> "${log_dir}/archives",
+		olddir_owner	=> 'root',
+		olddir_group	=> 'users',
+		olddir_mode     => '655',
+		create			=> '640 syslog adm',
+		postrotate		=> 'invoke-rc.d rsyslog reload',
+	}
 }
