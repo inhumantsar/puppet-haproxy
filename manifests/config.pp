dcc07f9c435058f7129c00020ecccbd2fc0330ef
Shaun Martin <shaunm@airg.com>
2014-07-22 21:12:16 +0000

    Parameterized config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,49 +1,57 @@
-class haproxy::config {
+class haproxy::config 
+(
+    $log_dir,
+    $config_dir,
+    $default_config_path,
+    $service_enable,
+    $service_user,
+    $service_group,
+)
+{
 
-  if $haproxy::log_dir != false {
-    file {$haproxy::log_dir :
-      ensure  => directory,
-      mode    => 664,
-      owner   => 'syslog',
-      group   => 'adm',
-    }
-  }
+	if $log_dir != false {
+		file { $log_dir :
+			ensure  => directory,
+			mode	=> 664,
+			owner	=> 'syslog',
+			group	=> 'adm',
+		}
+	}
 
-  file {"${haproxy::params::config_dir}/haproxy.cfg":
-    ensure  => present,
-    mode    => 664,
-    owner   => 'root',
-    group   => 'root',
-    require => Concat_build['haproxy']
-  }
+	file { "${config_dir}/haproxy.cfg":
+		ensure  => present,
+		mode	=> 664,
+		owner	=> 'root',
+		group	=> 'root',
+		require => Concat_build['haproxy']
+	}
 
-  concat_build { 'haproxy':
-    order   => ['*.tmp'],
-    target  => "${haproxy::params::config_dir}/haproxy.cfg",
-  }
+	concat_build { 'haproxy':
+		order   => ['*.tmp'],
+		target	=> "${config_dir}/haproxy.cfg",
+	}
 
-  concat_fragment { 'haproxy+001.tmp':
-    content => template('haproxy/haproxy_header.erb'),
-  }
+	concat_fragment { 'haproxy+001.tmp':
+		content => template('haproxy/haproxy_header.erb'),
+	}
 
-  $enabled = $haproxy::service_ensure? {
-    true    => 1,
-    running => 1,
-    default => 0
-  }
+	$enabled = $service_enable ? {
+		true	=> 1,
+		default => 0,
+	}
 
-  augeas { 'enable-haproxy':
-    context => "/files${haproxy::params::default_config}",
-    changes => [
-      "set ENABLED $enabled",
-    ]
-  }
+	augeas { 'enable-haproxy':
+		context => "/files${default_config_path}",
+		changes => [
+			"set ENABLED $enabled",
+		],
+	}
 
-  file { '/var/run/haproxy':
-    ensure  => directory,
-    mode    => '0755',
-    owner   => $haproxy::haproxy_user_group,
-    group   => $haproxy::haproxy_user_group
-  }
+	file { '/var/run/haproxy':
+		ensure  => directory,
+		mode	=> '0755',
+		owner	=> $service_user,
+		group	=> $service_group,
+	}
 
 }
