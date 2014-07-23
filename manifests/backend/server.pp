a4f7404655d4b9b990095a81b7a39cb7cc7d16d4
Shaun Martin <shaunm@airg.com>
2014-07-23 22:03:23 +0000

    Simplified parameters

--- manifests/backend/server.pp
+++ manifests/backend/server.pp
@@ -46,71 +46,32 @@
 # [*weight*]
 #   Weight to assign to server. interval 0(disabled) - 256 (maximum). Integer. Default: 100
 #
-define haproxy::backend::server (
+define haproxy::backend::server 
+(
   $backend_name,
-  $bind,
+  $host,
   $port,
   $file_template= 'haproxy/backend/server.erb',
   $server_name  = '',
-  $server_check = true,
-  $inter        = '5s',
-  $downinter    = '1s',
-  $fastinter    = '1s',
-  $rise         = 2,
-  $fall         = 3,
-  $backup       = false,
-  $send_proxy   = false,
-  $weight       = '100',
-) {
+  $params = '',
+) 
+{
 
   if !defined(Haproxy::Backend[$backend_name]) {
     fail ("No Haproxy::Backend[$backend_name] is defined!")
   }
 
-
-  if ( $bind !~ /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{2,5})?$/) {
-    fail('bind must contain a valid ip address, eventually followeb by :port. Ex: 192.168.1.1:80 or 192.168.1.1')
-  }
-
-  validate_bool($server_check)
-  validate_bool($backup)
-  validate_bool($send_proxy)
-
-  if !is_integer($rise) {
-    fail('rise parameter must be an integer value')
-  }
-
-  if !is_integer($fall) {
-    fail('fall parameter must be an integer value')
-  }
-
-  if !is_integer($weight) {
-    fail('weight parameter must be an integer value')
-  }
-
-  if $inter !~ /[0-9]{1,3}s/ {
-    fail('inter parameter must be an integer followed by s (seconds)')
-  }
-
-  if $downinter !~ /[0-9]{1,3}s/ {
-    fail('downinter parameter must be an integer followed by s (seconds)')
-  }
-
-  if $fastinter !~ /[0-9]{1,3}s/ {
-    fail('fastinter parameter must be an integer followed by s (seconds)')
-  }
-
-  $servername = $server_name ? {
+  $server_name = $server_name ? {
     ''      => $name,
     default => $server_name,
   }
 
-  $bind_address = $port? {
-    ''      => $bind,
-    default => "${bind}:${port}",
+  $host = $port ? {
+    ''      => $host,
+    default => "${host}:${port}",
   }
 
-  concat_fragment {"haproxy+002-${backend_name}-005-${name}.tmp":
+  concat_fragment {"haproxy+002-${backend_name}-005-${server_name}.tmp":
     content => template($file_template),
   }
 }
